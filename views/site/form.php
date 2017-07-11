835aba8b13dbe4fe018bec1756bdb15c3e9fed1f
biggdred <allijgibs@gmail.com>
2017-07-11 11:06:02 +0300

    add in view\site\form.php  submit

--- views/site/form.php
+++ views/site/form.php
@@ -1,8 +1,10 @@
 <?php
 
-use  yii\widgets\ActiveForm;
+use yii\helpers\Html;//include buttons
+use yii\widgets\ActiveForm;
 ?>
-<?php $f = ActiveForm::begin();?>
-    <?= $f->field($form, 'name')?>
-    <?= $f->field($form, 'email')?>
+<?php $f = ActiveForm::begin(); ?>
+    <?= $f->field($form, 'name') ?>
+    <?= $f->field($form, 'email') ?>
+    <?= Html::submitButton('Отправить') ?>
 <?php ActiveForm::end() ?>
