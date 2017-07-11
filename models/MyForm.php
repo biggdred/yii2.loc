6e87e0311d7de9330deef6aad3916a6652bc35b3
biggdred <allijgibs@gmail.com>
2017-07-11 10:36:26 +0300

    created in models\MyForm.php

---
+++ models/MyForm.php
@@ -0,0 +1,19 @@
+<?php
+
+class MyForm extends \yii\base\Model {
+    public $name;
+    public $email;
+
+    //правила rules
+    /**
+     *
+     */
+    public function rules(){
+        // name, email are required
+        return[
+            [['name','email'],'required']//name and email -заполнены
+            ,// email has to be a valid email address
+            ['email','email']
+            ];
+    }
+}
